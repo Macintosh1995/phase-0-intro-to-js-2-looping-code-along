c7b13e82c0c3d5c13d50e61146c7bab47cac6d87
Daniel <danmcdonald95@outlook.com>
2023-07-11 23:28:02 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names, happy = "happy") {
+    let card = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      card.push(`Thank you, ${names[i]}, for the wonderful ${happy} gift!`);
+    }
+    console.log(card);
+    return card;
+  }
+  
+  
+  function countDown(number){
+      let x =0
+      while (number >= x) {
+          console.log(number);
+          number--;
+          
+      }
+  
+  }
+  
+  countDown(10);
